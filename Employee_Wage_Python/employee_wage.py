b8ed9fbdc89677efeb0830c113537355e84c6796
unknown <mohit16gaikwad@gmail.com>
2022-07-20 09:24:07 +0530

    Added: Employee attendance checking

--- Employee_Wage_Python/employee_wage.py
+++ Employee_Wage_Python/employee_wage.py
@@ -1,1 +1,21 @@
-print("Hello")
+import random
+
+class EmployeeWage:
+    def employee(self):
+
+        try:
+            rand = random.randint(0, 1)
+            if rand == 0:
+                print("Employee is present")
+            else:
+                print("Employee is absent")
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    try:
+        obj = EmployeeWage()
+        obj.employee()
+    except Exception as e:
+        print(e)
